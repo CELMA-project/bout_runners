5765d178d61bd0e0f6167db5da176c47fc8eddd4
Michael Løiten <michael.l.magnussen@gmail.com>
2019-04-18 21:16:29 +0200

    Added omit to coverage and reincluded import test

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,15 +1,13 @@
 import unittest
-# from tests.conftest import *  # Add the python path
+from tests.conftest import *  # Add the python path
 from boututils.datafile import DataFile
 
 
 class TestImport(unittest.TestCase):
     def test_import(self):
- #       d = DataFile()
- #       self.assertIsInstance(d, DataFile)
-        pass
+        d = DataFile()
+        self.assertIsInstance(d, DataFile)
 
 
 if __name__ == '__main__':
     unittest.main()
-
