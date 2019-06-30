8402d24677af585bb60ba7cc2ff33cccdac1ecf2
loeiten <michael.l.magnussen@gmail.com>
2019-06-30 20:03:10 +0200

    Added tests to read makefile

--- tests/bout_runners/utils/test_read_make_file.py
+++ tests/bout_runners/utils/test_read_make_file.py
@@ -1,7 +1,8 @@
+import pytest
 from pathlib import Path
 from bout_runners.utils.read_make_file import ReadBoutMakeFile
 from bout_runners.utils.read_make_file import BoutMakeFileVariable
-
+from bout_runners.utils.read_make_file import ReadMakeFileError
 
 DATA_PATH = Path(__file__).absolute().parents[1].joinpath('data')
 
@@ -13,3 +14,30 @@ def test_read_bout_make_file():
 
     reader = ReadBoutMakeFile(DATA_PATH.joinpath('test_read'))
     assert reader.content == 'This is some text'
+
+
+@pytest.mark.parametrize("file,expected",
+                         [('Makefile_value', 'val 123 val.cxx.foo'),
+                          ('Makefile_multiple_value', 'not_val')])
+def test_get_variable_value(file, expected):
+    """
+    Tests that get_variable is reading variables properly
+    """
+
+    var = BoutMakeFileVariable(DATA_PATH.joinpath(file), 'VAR')
+    val = var.get_variable_value()
+
+    assert val == expected
+
+
+def test_get_variable_value_raises():
+    """
+    Tests that ReadMakeFileError is properly raised
+    """
+
+    var = BoutMakeFileVariable(DATA_PATH.joinpath(
+        'Makefile_only_comment'),
+        'VAR')
+
+    with pytest.raises(ReadMakeFileError):
+        var.get_variable_value()
