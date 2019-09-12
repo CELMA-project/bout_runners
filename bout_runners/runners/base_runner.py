5d7b431cfc4d20f1ff441c55585a81ca1f85d90a
loeiten <michael.l.magnussen@gmail.com>
2019-09-12 11:40:22 +0200

    Updated with exec path and bout path

--- bout_runners/runners/base_runner.py
+++ bout_runners/runners/base_runner.py
@@ -8,11 +8,21 @@ logger = logging.getLogger(__name__)
 # FIXME: Rudimentary runner to create database
 
 
-def single_run(path, nproc, options):
-    make = MakeProject(path)
+def single_run(execute_from_path,
+               bout_inp_dir=None,
+               nproc=None,
+               options=None):
+    make = MakeProject(execute_from_path)
     make.run_make()
 
     # FIXME: mpirun obtained from getmpirun in boututils. Still needed?
-    command = f'mpirun -np {nproc} ./{make.exec_name} {options}'
+    mpi_cmd = 'mpirun -np'
+    nproc_str = nproc if nproc is not None else 1
+    bout_inp_path_str =\
+        f' -d {bout_inp_dir}' if bout_inp_dir is not None else ''
+    options_str = f' {options}' if options is not None else ''
 
-    run_subprocess(command, path=path)
+    command = f'{mpi_cmd} {nproc_str} ./{make.exec_name}' \
+              f'{bout_inp_path_str}{options_str}'
+
+    run_subprocess(command, path=execute_from_path)
