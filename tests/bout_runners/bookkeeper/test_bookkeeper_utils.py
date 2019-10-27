3f89220931225d42414dc5aca679e1727853a4d0
Michael Løiten <michael.l.magnussen@gmail.com>
2019-10-27 14:51:35 +0100

    Added create table statement test

--- tests/bout_runners/bookkeeper/test_bookkeeper_utils.py
+++ tests/bout_runners/bookkeeper/test_bookkeeper_utils.py
@@ -2,6 +2,8 @@ from bout_runners.bookkeeper.bookkeeper_utils import \
     obtain_project_parameters
 from bout_runners.bookkeeper.bookkeeper_utils import \
     get_system_info_as_sql_type
+from bout_runners.bookkeeper.bookkeeper_utils import \
+    get_create_table_statement
 
 
 def test_obtain_project_parameters(get_test_data_path):
@@ -16,3 +18,26 @@ def test_obtain_project_parameters(get_test_data_path):
 def test_get_system_info_as_sql_type():
     sys_info_dict = get_system_info_as_sql_type()
     assert isinstance(sys_info_dict, dict)
+
+
+def test_get_create_table_statement():
+    result = get_create_table_statement(
+        name='foo',
+        columns=dict(bar='baz',
+                     foobar='qux'),
+        primary_key='quux',
+        foreign_keys=dict(quuz=('corge', 'grault'),
+                          garply=('waldo', 'fred')))
+
+    expected = ('CREATE TABLE foo \n'
+                '(   quux INTEGER PRIMARY KEY,\n'
+                '    bar baz,\n'
+                '    foobar qux,\n'
+                '    quuz INTEGER,\n'
+                '    garply INTEGER,\n'
+                '    FOREIGN KEY(quuz) \n'
+                '        REFERENCES corge(grault),\n'
+                '    FOREIGN KEY(garply) \n'
+                '        REFERENCES waldo(fred))')
+
+    assert result == expected
