e2ee4b9683e83e74e14fc17220030a7ec1c0b083
loeiten <michael.l.magnussen@gmail.com>
2020-05-01 17:14:46 +0200

    Moved faulty pylint disabler

--- tests/unit/bout_runners/database/test_database_writer.py
+++ tests/unit/bout_runners/database/test_database_writer.py
@@ -43,4 +43,5 @@ def test_database_writer(make_test_schema):
     assert (values == np.dtype('int64')).all()
 
     for key, value in dummy_split_dict.items():
-        assert table.loc[0, key] == value  # pylint: disable=no-member
+        # pylint: disable=no-member
+        assert table.loc[0, key] == value
