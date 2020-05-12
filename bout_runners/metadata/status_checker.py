247f30073e6636224a3958cadaec5924eb3c9d6f
loeiten <michael.l.magnussen@gmail.com>
2020-05-12 08:59:18 +0200

    Updated StatusChecker

--- bout_runners/metadata/status_checker.py
+++ bout_runners/metadata/status_checker.py
@@ -2,6 +2,7 @@
 
 
 import logging
+import psutil
 from bout_runners.database.database_reader import DatabaseReader
 from bout_runners.log.log_reader import LogReader
 from bout_runners.metadata.metadata_updater import MetadataUpdater
@@ -11,17 +12,10 @@ class StatusChecker:
     """
     FIXME
     """
-    # Whenever we are reading from database we would like to update,
-    # however not so nice to mix reader and writer through the status
-    # checker
-    # Is this rather status updater?
-    # An alternative could be to put it in metadatawriter reader and
-    # writer
-    # Can also be called independently
 
     def __init__(self, database_connector, bout_paths):
         """
-        FIXME
+        Setup the status checker with a connector, reader and paths
 
         Notes
         -----
@@ -31,103 +25,148 @@ class StatusChecker:
 
         Parameters
         ----------
-        database_connector
-        bout_paths
+        database_connector : DatabaseConnector
+            Connection to the database
+        bout_paths : BoutPaths
+            Object containing paths to the project
         """
         self.__database_connector = database_connector
         self.__database_reader = \
             DatabaseReader(self.__database_connector)
         self.__bout_paths = bout_paths
 
-    def check_status(self):
-        """FIXME"""
+    def check_and_update_status(self):
+        """Check and update the status for the schema."""
         # Check that run table exist
         if not self.__database_reader.check_tables_created():
-            logging.warning(
+            logging.error(
                 'No tables found in %s',
                 self.__database_reader.database_connector.database_path)
-            # FIXME: What should the exit be here?
+            message = ('Can not check the status of schemas that does '
+                       'not exist')
+            raise RuntimeError(message)
 
+        # Create place holder metadata_updater
+        metadata_updater = \
+            MetadataUpdater(self.__database_connector, run_id=-1)
+
+        # Check runs with status 'submitted'
         query = (
             "SELECT name, id AS run_id FROM run WHERE\n"
             "latest_status = 'submitted'")
         submitted_to_check = self.__database_reader.query(query)
+        self.__check_submitted(metadata_updater,
+                               submitted_to_check)
 
-        latest_status = 'submitted'
+        # Check runs with status 'running'
+        query = (
+            "SELECT name FROM run WHERE\n"
+            "latest_status = 'running'")
+        running_to_check = self.__database_reader.query(query)
+        self.__check_running(metadata_updater, running_to_check)
 
-        metadata_updater = \
-            MetadataUpdater(self.__database_connector, run_id=-1)
+    def __check_submitted(self, metadata_updater, submitted_to_check):
+        """
+        Check the status of all runs which has status `submitted`.
 
+        Parameters
+        ----------
+        metadata_updater : MetadataUpdater
+            Object which updates the database
+        submitted_to_check : DataFrame
+            DataFrame containing the `id` and `name` of the runs with
+            status `submitted`
+        """
         for name, run_id in submitted_to_check.itertuples(index=False):
             metadata_updater.run_id = run_id
 
             log_path = self.__bout_paths.joinpath(name, 'BOUT.log.0')
             log_reader = LogReader(log_path)
+
             if log_path.is_file():
                 if log_reader.started():
                     start_time = log_reader.start_time
                     metadata_updater.update_start_time(start_time)
-                    if log_reader.ended():
-                        end_time = log_reader.end_time
-                        metadata_updater.update_stop_time(end_time)
-                        latest_status = 'complete'
-                    else:
-                        # Check if the process is still running
-                        # FIXME: You are here
-                        pid = log_reader.pid
-                        # FIXME: Consider using pid, else
-                        #  https://stackoverflow.com/questions/568271/how-to-check-if-there-exists-a-process-with-a-given-pid-in-python
-                        if exist_pid(pid):
-                            latest_status = 'running'
-                        else:
-                            # FIXME: Check that the process didn't
-                            #  errored or finished in the meantime
-                            latest_status = 'error'
+                    latest_status = \
+                        self.__check_if_stopped(log_reader,
+                                                metadata_updater)
+
                 else:
                     # No started time is found in the log
-                    if log_reader.pid_exist():
-                        # FIXME: This is the same as above
-                        pid = log_reader.pid
-                        if exist_pid(pid):
-                            latest_status = 'running'
-                        else:
-                            # FIXME: Check that the process didn't
-                            #  errored or finished in the meantime
-                            latest_status = 'error'
-                    else:
-                        # FIXME: Wait and check again, if fails ->
-                        #  possible crash. If possible_crash,
-                        #  this should also be queried for
-                        pass
+                    latest_status = \
+                        self.check_if_running_or_errored(log_reader)
             else:
                 # No log file exists
-                # FIXME: Either queued or failed...how to find out if
-                #  queued? If queued -> need to check for these
-                pass
-        # FIXME: You are here
+                # NOTE: This means that the execution is either in a
+                #       queue or has failed the submission.
+                #       For now, we still consider this as submitted
+                latest_status = 'submitted'
 
-        metadata_updater.update_latest_status(latest_status)
+            metadata_updater.update_latest_status(latest_status)
 
-        query = (
-            "SELECT name FROM run WHERE\n"
-            "latest_status = 'running'")
-        running_to_check = self.__database_reader.query(query)
-        # Use DatabaseReader to check for status submitted or
-        #    possible_crash in run table
-        # From there get dir to check in
-        # Check for BOUT.log.0
-        # If BOUT.log.0 exist -> can search for pid
-        #     Can check if pid start time is somewhat equal to
-        #     submitted time?
-        #     Possibility to use psutil
-        # If not found, status is submitted
-        #    Can also be in queue?
-        # If file found and not found time started -> crashed before
-        #    status possible crashed
-        # If found, check for started time in log
-        #
-        pass
+    def __check_running(self, metadata_updater, running_to_check):
+        """
+        Check the status of all runs which has status `running`.
 
-        # Data files: BOUT.log.0
-        # If a job has been canceled, the log is just short
-        #
+        Parameters
+        ----------
+        metadata_updater : MetadataUpdater
+            Object which updates the database
+        running_to_check : DataFrame
+            DataFrame containing the `id` and `name` of the runs with
+            status `running`
+        """
+        for name, run_id in running_to_check.itertuples(index=False):
+            metadata_updater.run_id = run_id
+            log_path = self.__bout_paths.joinpath(name, 'BOUT.log.0')
+            log_reader = LogReader(log_path)
+            latest_status = self.check_if_running_or_errored(log_reader)
+            metadata_updater.update_latest_status(latest_status)
+
+    def __check_if_stopped(self, log_reader, metadata_updater):
+        """
+        Check if a run is still running or has errored.
+
+        Parameters
+        ----------
+        log_reader : LogReader
+            The object which reads log files
+        metadata_updater : MetadataUpdater
+            Object which updates the database
+
+        Returns
+        -------
+        latest_status : 'complete' or 'running' or 'error'
+            The latest status
+        """
+        if log_reader.ended():
+            end_time = log_reader.end_time
+            metadata_updater.update_stop_time(end_time)
+            latest_status = 'complete'
+        else:
+            latest_status = \
+                self.check_if_running_or_errored(log_reader)
+        return latest_status
+
+    @staticmethod
+    def check_if_running_or_errored(log_reader):
+        """
+        Check if a run is still running or has errored.
+
+        Parameters
+        ----------
+        log_reader : LogReader
+            The object which reads log files
+
+        Returns
+        -------
+        latest_status : 'running' or 'error'
+            The latest status
+        """
+        pid = log_reader.pid
+        if psutil.pid_exists(pid):
+            latest_status = 'running'
+        else:
+            latest_status = 'error'
+        return latest_status
+
