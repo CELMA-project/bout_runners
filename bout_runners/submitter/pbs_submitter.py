2a90664326cdf766caa8fa183fa14fd5996f1364
Michael Løiten <michael.l.magnussen@gmail.com>
2020-10-08 14:01:38 +0200

    Checkpoint: Added qsub command

--- bout_runners/submitter/pbs_submitter.py
+++ bout_runners/submitter/pbs_submitter.py
@@ -3,9 +3,10 @@
 import logging
 
 from pathlib import Path
-from typing import Optional, Dict
+from typing import Optional, Dict, List, Tuple
 
 from bout_runners.submitter.processor_split import ProcessorSplit
+from bout_runners.submitter.local_submitter import LocalSubmitter
 from bout_runners.submitter.abstract_submitters import (
     AbstractSubmitter,
     AbstractClusterSubmitter,
@@ -24,6 +25,8 @@ class PBSSubmitter(AbstractSubmitter, AbstractClusterSubmitter):
     ):
         """
         Set the member data.
+
+        # FIXME: self.__status from LocalSubmitter should be in the parent class
 
         Parameters
         ----------
@@ -50,6 +53,9 @@ class PBSSubmitter(AbstractSubmitter, AbstractClusterSubmitter):
             self.__submission_dict["walltime"] = self.structure_time_to_pbs_format(
                 self.__submission_dict["walltime"]
             )
+        self.__waiting_for: List[str] = list()
+        self.__submission_id: Optional[str] = None
+        self.__pid: Optional[int] = None
 
     @staticmethod
     def structure_time_to_pbs_format(time_str: str) -> str:
@@ -79,6 +85,32 @@ class PBSSubmitter(AbstractSubmitter, AbstractClusterSubmitter):
         hours += days * 24
         return f"{hours}:{minutes}:{seconds}"
 
+    @property
+    def waiting_for(self) -> Tuple[str, ...]:
+        """
+        Return the waiting for list as a tuple.
+
+        Returns
+        -------
+        tuple of str
+            The waiting for list as a tuple
+        """
+        return tuple(self.__waiting_for)
+
+    def add_waiting_for(self, waiting_for_id: str) -> None:
+        """
+        Add a waiting for id to the waiting for list.
+
+        This will waiting for list will be written to the submission string
+        upon creation
+
+        Parameters
+        ----------
+        waiting_for_id : str
+            Id to the job waiting for
+        """
+        self.__waiting_for.append(waiting_for_id)
+
     def create_submission_string(self, command: str) -> str:
         """
         Create the core of a PBS script as a string.
@@ -100,6 +132,11 @@ class PBSSubmitter(AbstractSubmitter, AbstractClusterSubmitter):
         queue = self.__submission_dict["queue"]
         mail = self.__submission_dict["mail"]
         log_and_error_base = self.__job_name
+        waiting_for_str = (
+            f"#PBS -W depend=afterok:{':'.join(self.waiting_for)}{newline}"
+            if len(self.waiting_for) != 0
+            else ""
+        )
         job_string = (
             "#!/bin/bash\n"
             f"#PBS -N {self.__job_name}\n"
@@ -114,12 +151,18 @@ class PBSSubmitter(AbstractSubmitter, AbstractClusterSubmitter):
             # a=abort b=begin e=end
             f"{'#PBS -m abe{newline}' if mail is not None else ''}"
             f"{'#PBS -M {mail}{newline}' if mail is not None else ''}"
+            f"{waiting_for_str}"
             "\n"
             # Change directory to the directory of this script
             "cd $PBS_O_WORKDIR\n"
             f"{command}"
         )
 
+        waiting_for_str = (
+            f"#SBATCH --dependency=afterok:{':'.join(self.waiting_for)}{newline}"
+            if len(self.waiting_for) != 0
+            else ""
+        )
         job_string = (
             "#!/bin/bash\n"
             f"#SBATCH --job-name={self.__job_name}\n"
@@ -133,6 +176,7 @@ class PBSSubmitter(AbstractSubmitter, AbstractClusterSubmitter):
             f"#SBATCH -e {log_and_error_base}.err\n"
             f"{'#SBATCH --mail-type=ALL{newline}' if mail is not None else ''}"
             f"{'#SBATCH --mail-user={mail}{newline}' if mail is not None else ''}"
+            f"{waiting_for_str}"
             "\n"
             # Change directory to the directory of this script
             "cd $SLURM_SUBMIT_DIR\n"
@@ -145,31 +189,79 @@ class PBSSubmitter(AbstractSubmitter, AbstractClusterSubmitter):
         """
         Submit a command.
 
+        FIXME: 4346 in bout-dev
+        FIXME: Before sending command to this function one must inject the dependency
+
         Parameters
         ----------
         command : str
             Command to submit
         """
+        script_path = self.__store_path.joinpath(f"{self.__job_name}.sh")
+        with script_path.open("w") as file:
+            file.write(self.create_submission_string(command))
+
+        # Submit the command through a local submitter
+        local_submitter = LocalSubmitter(run_path=self.__store_path)
+        local_submitter.submit_command(f"qsub {script_path}")
+        self.__pid = local_submitter.pid
+        local_submitter.wait_until_completed()
+        self.__submission_id = local_submitter.std_out
 
     @property
     def pid(self) -> Optional[int]:
-        """Return the process id."""
+        """
+        Return the process id.
+
+        FIXME: This could be removed as we are interested in submission id
+               rather than pid
+        """
+        return self.__pid
+
+    @property
+    def submission_id(self) -> Optional[str]:
+        """
+        Return the submission id.
+
+        Returns
+        -------
+        self.__submission_id : None or str
+            The job id given the process by the cluster
+            None is given if the process has not been submitted
+        """
+        return self.__submission_id
 
     @property
     def std_out(self) -> Optional[str]:
-        """Return the standard output."""
+        """
+        Return the standard output.
+
+        FIXME: Can be the tail from the .log
+        """
 
     @property
     def std_err(self) -> Optional[str]:
-        """Return the standard error."""
+        """
+        Return the standard error.
+
+        FIXME: Can be the tail from the .err
+        """
 
     @property
     def return_code(self) -> Optional[int]:
-        """Return the return code."""
+        r"""
+        Return the return code.
+
+        FIXME: Must be obtained from qstat or the like?
+        FIXME: You are here: Use `jobtrace pbs_id` regex Exit_status=(\d*)
+        https://stackoverflow.com/questions/24248735/how-get-information-of-completed-pbs-or-torque-jobs/35511337
+        """
 
     def wait_until_completed(self, raise_error: bool = True) -> None:
         """
         Wait until the process has completed.
+
+        FIXME
 
         Parameters
         ----------
